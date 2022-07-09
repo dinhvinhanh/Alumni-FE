21b4f00d13ef2a3e12a8245a190969ed70716a19
huuvuno1 <nguyenhuuvuno1@gmail.com>
2022-07-10 01:36:00 +0700

    feat: add footer component

---
+++ src/app/components/Footer/index.jsx
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import Grid from '@mui/material/Grid';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import EmailIcon from '@mui/icons-material/Email';
+import { Box } from '@mui/material';
+import useStyles from './styles';
+import { Mail } from '@mui/icons-material';
+
+export default function Footer() {
+  const classes = useStyles();
+  return (
+    <footer className={' text-white mt-10'}>
+      <Box style={{ display: 'flex', justifyContent: 'center' }} className={'bg-red-900 rounded-tl-full rounded-tr-full'}>
+        <Grid container width={1200} spacing={12} paddingY={5}>
+          <Grid item xl={2}>
+            <img src={'https://i3.connections.vn/alumni.hanu.vn/img/logo.png?resize=80,80&quality=100&strip=all'} alt=''/>
+          </Grid>
+          <Grid item xl={6}>
+            <h1 className="uppercase mb-3">Mạng lưới cựu sinh viên</h1>
+            <p className={'text-sm text-blue-50 leading-7'}>Trường đại học công nghệ thông tin</p>
+            <p className={'text-sm text-blue-50 leading-7'}>Địa chỉ: ksdf sdf ksdf asdflsadjf sdf </p>
+            <p className={'text-sm text-blue-50 leading-7'}>Điện thoại: ksdf sdf ksdf asdflsadjf sdf </p>
+            <p className={'text-sm text-blue-50 leading-7'}>Fax: (84-24) 38345 4545 45</p>
+            <p className={'text-sm text-blue-50 leading-7'}>Email: meo@meo.com</p>
+          </Grid>
+          <Grid item xl={2}>
+            <a href="/"><p className={'text-sm text-blue-50 leading-7'}>Giới thiệu</p></a>
+            <p className={'text-sm text-blue-50 leading-7'}>Tin tức - Sự kiện</p>
+            <p className={'text-sm text-blue-50 leading-7'}>Gương mặt cựu sinh viên</p>
+          </Grid>
+          <Grid item xl={2}>
+            <p className={'text-sm text-blue-50 leading-7'}>Điện thoại: ksdf sdf ksdf asdflsadjf sdf </p>
+            <p className={'text-sm text-blue-50 leading-7'}>Fax: (84-24) 38345 4545 45</p>
+            <p className={'text-sm text-blue-50 leading-7'}>Email: meo@meo.com</p>
+          </Grid>
+        </Grid>
+      </Box>
+      <div className={'bg-red-900'}>
+        <div
+          className={'flex justify-between mx-auto border-t-2 p-4 border-white'}
+          style={{ width: '1300px' }}
+        >
+          <p>	&copy; 2022 - Trường Đại học Quốc Gia, All Rights Reserved |
+            <span className={'animate-pulse'}> Powered by Đinh Vĩnh Anh</span>
+          </p>
+          <div className={'flex'}>
+            <FacebookIcon fontSize='large' />
+            <EmailIcon fontSize='large' className={'ml-4'}/>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
